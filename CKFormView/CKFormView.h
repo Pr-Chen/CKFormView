9328fed8d9836dbe720cd9032910e8c4e7c5fe00
陈凯 <chen526110979@qq.com>
2017-02-27 14:55:52 +0800

    添加了demo和注释

--- CKFormView/CKFormView.h
+++ CKFormView/CKFormView.h
@@ -18,31 +18,66 @@ typedef struct {
 
 @optional
 #pragma mark - 数据源
+//行数
 - (NSInteger)numberOfRowsInFormView:(CKFormView *)formView;
+
+//列数
 - (NSInteger)formView:(CKFormView *)formView numberOfColumnsForRow:(NSInteger)row;
+
+//每个单元格要显示的内容
+- (NSString *)formView:(CKFormView *)formView textForPosition:(CKFormCellPosition)position;
+
+#pragma mark - 样式
+//行高
 - (CGFloat)formView:(CKFormView *)formView heightForRow:(NSInteger)row;
+
+//列宽
 - (CGFloat)formView:(CKFormView *)formView widthForPosition:(CKFormCellPosition)position;
 
-- (NSString *)formView:(CKFormView *)formView textForPosition:(CKFormCellPosition)position;
-#pragma mark - 样式
+//文字对齐方式
 - (NSTextAlignment)formView:(CKFormView *)formView textAlignmentForPosition:(CKFormCellPosition)position;
+
+//每个单元格的字体
 - (UIFont *)formView:(CKFormView *)formView fontForPosition:(CKFormCellPosition)position;
+
+//文字颜色
 - (UIColor *)formView:(CKFormView *)formView textColorForPosition:(CKFormCellPosition)position;
+
+//背景颜色
 - (UIColor *)formView:(CKFormView *)formView backgroundColorForPosition:(CKFormCellPosition)position;
 
+//竖直分割线的宽度
 - (CGFloat)formView:(CKFormView *)formView verticalLineWidthForPosition:(CKFormCellPosition)position;
+
+//竖直分割线的颜色
 - (UIColor *)formView:(CKFormView *)formView verticalLineColorForPosition:(CKFormCellPosition)position;
+
+//水平分割线的宽度
 - (CGFloat)formView:(CKFormView *)formView bottomLineWidthForRow:(NSInteger)row;
+
+//水平分割线的颜色
 - (UIColor *)formView:(CKFormView *)formView bottomLineColorForRow:(NSInteger)row;
 
+//键盘类型
 - (UIKeyboardType)formView:(CKFormView *)formView keyboardTypeForPosition:(CKFormCellPosition)position;
 
 #pragma mark - 编辑
+//是否可编辑
 - (BOOL)formView:(CKFormView *)formView editableForPosition:(CKFormCellPosition)position;
+
+//是否应该进入编辑状态
 - (BOOL)formView:(CKFormView *)formView shouldBeginEditingForPosition:(CKFormCellPosition)position;
+
+//已经开始编辑
 - (void)formView:(CKFormView *)formView didBeginEditingForPosition:(CKFormCellPosition)position;
+
+//结束编辑
 - (void)formView:(CKFormView *)formView didEndEditingForPosition:(CKFormCellPosition)position;
+
+//内容已改变
 - (void)formView:(CKFormView *)formView didChangeTextForPosition:(CKFormCellPosition)position;
+
+//内容是否应该变化
 - (BOOL)formView:(CKFormView *)formView shouldChangeCharactersWithReplacementString:(NSString *)string forPosition:(CKFormCellPosition)position;
 //键盘弹出后,编辑行与键盘顶部的距离.负数代表键盘遮挡了编辑行
 - (void)formView:(CKFormView *)formView edittingRowBottomToKeyboardTopSpacing:(CGFloat)spacing;
@@ -55,36 +90,49 @@ typedef struct {
 
 @property (strong, nonatomic) UITableView *tableView;
 
+/*
+设置这4个属性,则无需在代理方法中设置
+*/
 @property (assign, nonatomic) NSInteger numberOfRows;
 @property (assign, nonatomic) NSInteger numberOfColumns;
 @property (assign, nonatomic) CGFloat rowHeight;
 @property (assign, nonatomic) CGFloat columnWidth;
-//表头的高
+
+//可以设置一个滚动时,固定在顶部的表头
 @property (assign, nonatomic) CGFloat headerHeight;
 @property (strong, nonatomic) UIView *headerView;
 
+//竖直分割线的颜色
 @property (strong, nonatomic) UIColor *verticalLineColor;
+//竖直分割线的宽度
 @property (assign, nonatomic) CGFloat verticalLineWidth;
-
+//水平分割线的颜色
 @property (strong, nonatomic) UIColor *horizontalLineColor;
+//水平分割线的宽度
 @property (assign, nonatomic) CGFloat horizontalLineWidth;
-
+//表格边框的颜色
 @property (strong, nonatomic) UIColor *borderColor;
+//表格边框的宽度
 @property (assign, nonatomic) CGFloat borderWidth;
 
+//单元格中文字对齐方式
 @property (assign, nonatomic) NSTextAlignment textAlignment;
+//单元格中文字字体
 @property (strong, nonatomic) UIFont *textFont;
+//单元格中文字颜色
 @property (strong, nonatomic) UIColor *textColor;
 //单元格背景
 @property (strong, nonatomic) UIColor *cellBackgroundColor;
 
 //是否可编辑
 @property (assign, nonatomic) BOOL editable;
+//是否有弹性效果(行数较少显示全部行的时候可以设置为NO)
 @property (assign, nonatomic) BOOL bounces;
+//是否正在编辑
 @property (assign, nonatomic) BOOL isEditing;
 //最后一条横线是否隐藏
 @property (assign, nonatomic) BOOL lastHorizontalLineHidden;
-
+//能否滚动
 @property (nonatomic, assign) BOOL scrollEnabled;
 @property (nonatomic, assign) CGPoint contentOffset;
 @property (nonatomic, assign, readonly) CGSize contentSize;
@@ -93,7 +141,7 @@ typedef struct {
 
 - (NSString *)textForPosition:(CKFormCellPosition)position;
 - (void)setText:(NSString *)text forPosition:(CKFormCellPosition)position;
-
+//重载数据
 - (void)reloadData;
 
 @end
--- ViewController.m
+++ ViewController.m
@@ -9,9 +9,11 @@
 #import "ViewController.h"
 #import "CKFormView.h"
 
-@interface ViewController ()
+@interface ViewController () <CKFormViewDelegate>
 
 @property (strong, nonatomic) CKFormView *formView;
+@property (strong, nonatomic) NSArray *orders;
+@property (strong, nonatomic) NSArray *titles;
 
 @end
 
@@ -20,20 +22,101 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.titles = @[@"款号", @"类别", @"单价", @"颜色", @"数量", @"金额"];
+    self.orders = @[
+                    @{@"款号":@"JF111111", @"类别":@"短外套", @"单价":@"1000", @"颜色":@"黑色", @"数量":@"2", @"金额":@"2000"},
+                    @{@"款号":@"JF111112", @"类别":@"衬衫", @"单价":@"2000", @"颜色":@"红色", @"数量":@"3", @"金额":@"6000"},
+                    @{@"款号":@"JF111113", @"类别":@"衬衫", @"单价":@"3000", @"颜色":@"绿色", @"数量":@"1", @"金额":@"3000"},
+                    @{@"款号":@"JF111114", @"类别":@"连衣裙", @"单价":@"4000", @"颜色":@"蓝色", @"数量":@"4", @"金额":@"16000"},
+                    @{@"款号":@"JF111115", @"类别":@"风衣", @"单价":@"5000", @"颜色":@"白色", @"数量":@"5", @"金额":@"25000"},
+                    @{@"款号":@"JF111116", @"类别":@"连衣裙", @"单价":@"1000", @"颜色":@"印花", @"数量":@"2", @"金额":@"2000"},
+                    @{@"款号":@"JF111117", @"类别":@"西服", @"单价":@"2000", @"颜色":@"橘色", @"数量":@"3", @"金额":@"6000"},
+                    @{@"款号":@"JF111118", @"类别":@"吊带", @"单价":@"300", @"颜色":@"米色", @"数量":@"1", @"金额":@"300"},
+                    @{@"款号":@"JF111119", @"类别":@"T恤", @"单价":@"400", @"颜色":@"黑色", @"数量":@"4", @"金额":@"1600"},
+                    @{@"款号":@"JF111120", @"类别":@"针织衫", @"单价":@"3000", @"颜色":@"白色", @"数量":@"2", @"金额":@"6000"},
+                    ];
+    
     self.formView = [[CKFormView alloc] initWithFrame:CGRectZero];
-    self.formView.numberOfRows = 15;
-    self.formView.numberOfColumns = 5;
     self.formView.verticalLineWidth = 0.5;
     self.formView.horizontalLineWidth = 0.5;
     self.formView.borderWidth = 0.5;
+    self.formView.delegate = self;
     [self.view addSubview:self.formView];
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    self.formView.frame = CGRectInset(self.view.bounds, 25, 25);
-    self.formView.columnWidth = self.formView.frame.size.width/5;
-    self.formView.rowHeight = self.formView.frame.size.height/15;
+    self.formView.frame = CGRectMake(25, 64, self.view.bounds.size.width-50, self.formView.rowHeight*(self.orders.count+1)+2*self.formView.borderWidth);
+    
+}
+
+- (NSInteger)numberOfRowsInFormView:(CKFormView *)formView {
+    return self.orders.count+1;
+}
+
+- (NSInteger)formView:(CKFormView *)formView numberOfColumnsForRow:(NSInteger)row {
+    return 6;
+}
+
+- (NSString *)formView:(CKFormView *)formView textForPosition:(CKFormCellPosition)position {
+    if (position.row==0) {
+        return self.titles[position.column];
+    }
+    NSString *title = self.titles[position.column];
+    return self.orders[position.row-1][title];
+}
+
+- (CGFloat)formView:(CKFormView *)formView widthForPosition:(CKFormCellPosition)position {
+    switch (position.column) {
+        case 0:
+        return 0.2*formView.bounds.size.width;
+        break;
+        case 1:
+        return 0.15*formView.bounds.size.width;
+        break;
+        case 2:
+        return 0.15*formView.bounds.size.width;
+        break;
+        case 3:
+        return 0.15*formView.bounds.size.width;
+        break;
+        case 4:
+        return 0.15*formView.bounds.size.width;
+        break;
+        default:
+        return 0.2*formView.bounds.size.width;
+        break;
+    }
+}
+
+- (UIColor *)formView:(CKFormView *)formView backgroundColorForPosition:(CKFormCellPosition)position {
+    if (position.row==0) {
+        return [UIColor colorWithWhite:0.95 alpha:1];
+    }
+    return [UIColor whiteColor];
+}
+
+- (BOOL)formView:(CKFormView *)formView editableForPosition:(CKFormCellPosition)position {
+    if (position.row == 0) {
+        return NO;
+    }
+    else if (position.column == 4) {
+        return YES;
+    }
+    return NO;
+}
+
+- (BOOL)formView:(CKFormView *)formView shouldChangeCharactersWithReplacementString:(NSString *)string forPosition:(CKFormCellPosition)position {
+    if ([string isEqualToString:@"\n"]) {
+        [formView endEditing:YES];
+        return NO;
+    }
+    return YES;
+}
+
+- (void)formView:(CKFormView *)formView didEndEditingForPosition:(CKFormCellPosition)position {
+    NSString *text = [formView textForPosition:position];
+    NSLog(@"单元格的内容为: %@", text);
 }
 
 
